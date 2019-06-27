abcbb6ff0e4f2b55a0853e6c1ea1abc8f36c5358
Carlo Frauenfelder <frauenfelder@medialogic.it>
2019-06-27 12:56:28 +0200

    add two function

--- db_backup_library.php
+++ db_backup_library.php
@@ -22,209 +22,254 @@
 				2nd: file Name Default Name is backup_yyy-mm-dd
 		--db_import(source)
 			Usage: If you want to Import Database from SQL File Use this method
-			--Parameters
-			1st: Source/Path of SQL file
-*/			
+			--Parameters			1st: Source/Path of SQL file
+
+*/
 
 
-class db_backup{
-		private $exported_database;
-		
-		public function backup($conn){
-			/*-------------------------------------*/
-			//------Creating Table SQL start-------//
-			/*-------------------------------------*/
+class db_backup
+{
+    private $exported_database;
 
-			$table_sql = array();
-			foreach ($this->tables($conn) as $key => $table) {
-				$tbl_query=mysqli_query($conn,"SHOW CREATE TABLE ".$table);
-				$row2 = mysqli_fetch_row($tbl_query);
-				$table_sql[]=$row2[1];
-			}
-			
-			$solid_tablecreate_sql = implode("; \n\n" , $table_sql);
-			/*-------------------------------------*/
-			//-------Creating Table SQL end--------//
-			/*-------------------------------------*/
+    public function lineImport($conn, $fileName)
+    {
+        $lines = file($fileName);
+        $result = false;
+        foreach ($lines as $line) {
+            $result = (mysqli_query($conn, $line) == true)? true : false;
+        }
+        return $result;
+    }
 
+    public function backup($conn)
+    {
+        /*-------------------------------------*/
+        //------Creating Table SQL start-------//
+        /*-------------------------------------*/
 
-			/*-------------------------------------*/
-			//------Inserting Data SQL Start-------//
-			/*-------------------------------------*/
-			$all_table_data=array();
-			foreach ($this->tables($conn) as $key => $table) {
-				$show_field=$this->view_fields($conn,$table);
-				$solid_field_name=implode(", ",$show_field);
-				$create_field_sql="INSERT INTO `$table` ( ".$solid_field_name.") VALUES \n";
+        $table_sql = array();
+        foreach ($this->tables($conn) as $key => $table) {
+            $tbl_query = mysqli_query($conn, "SHOW CREATE TABLE " . $table);
+            $row2 = mysqli_fetch_row($tbl_query);
+            $table_sql[] = $row2[1];
+        }
 
-				//Start checking data available
-				mysqli_query($conn,"SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
-				$table_data= mysqli_query($conn,"SELECT*FROM ".$table);
-				if(!$table_data){
-					echo 'Could not run query: '. mysqli_error($conn);
-				}
-				
-				if (mysqli_num_rows( $table_data) > 0) {
-					$data_viewig=$this->view_data($conn,$table);
-					$splice_data = array_chunk($data_viewig,50);
-					foreach($splice_data as $each_datas){
-						$solid_data_viewig=implode(", \n",$each_datas)."; ";
-						$all_table_data[]=$create_field_sql.$solid_data_viewig;
-					}
-				
-				}
-				else{
-					$all_table_data[]=null;
-				}
-				//End checking data available
-				
-				
-				
-			}
-			$entiar_table_data=implode(" \n\n\n",$all_table_data);
-			/*-------------------------------------*/
-			//-------Inserting Data SQL End--------//
-			/*-------------------------------------*/
-			$this->exported_database=$solid_tablecreate_sql."; \n \n".$entiar_table_data;
-			return $this;
-		}
-
-		//Additional Methods
-		/*-------------------------------------*/
-		//--------Functions Start here---------//
-		/*-------------------------------------*/
-		
-		public function download($name='backup'){
-			/*//Download
-			$file_name="Tmpdata.sql";
-			$file=fopen($file_name,"w+");
-			fwrite($file, $this->exported_database);*/
-			
-			header('Content-Type: application/sql');
-			header('Content-Disposition: attachment; filename='.$name.'.sql');
-			echo $this->exported_database;
-			/*readfile($file_name);
-			fclose($file);
-			unlink($file_name);*/
-		}
-		
-		public function save($path,$name=""){
-			$name = ($name != "") ? $name : 'backup_' . date('Y-m-d');
-			
-			//Save file
-			$file = fopen($path.$name.".sql","w+");
-			$fw = fwrite($file, $this->exported_database);	
-			if(!$fw){
-				return false;
-			}
-			else {
-				return true; 
-			}
-		}
-		
-		public function connect($server,$user,$pass,$db){
-			$conn=mysqli_connect($server,$user,$pass,$db);
-
-			if(!$conn){
-				echo mysqli_error($conn);
-			}
-				return $conn;
-
-		}
-	
-		public function tables($conn){
-			/*-------------------------------------*/
-			//------Creating Table List start------//
-			/*-------------------------------------*/
-			$tb_name=mysqli_query($conn,"SHOW TABLES");
-			$tables=array();
-			while ($tb=mysqli_fetch_row($tb_name)) {
-				$tables[]=$tb[0] ;
-			}
-			/*-------------------------------------*/
-			//-------Creating Table List end-------//
-			/*-------------------------------------*/
-			return $tables;
-		}
-		
-		public function view_fields($conn,$tablename){
-			$all_fields=array();
-			$fields = mysqli_query($conn,"SHOW COLUMNS FROM ".$tablename);
-			if (!$fields) {
-			 echo 'Could not run query: ' . mysqli_error($conn);
-			}
-			
-			if (mysqli_num_rows($fields) > 0) {
-				while ($field = mysqli_fetch_assoc($fields)) {
-					$all_fields[]="`".$field["Field"]."`";
-				}
-			}
-			return $all_fields;
-		}
+        $solid_tablecreate_sql = implode("; \n\n", $table_sql);
+        /*-------------------------------------*/
+        //-------Creating Table SQL end--------//
+        /*-------------------------------------*/
 
 
-		public function view_data($conn,$tablename){
-			$all_data=array();
-			$table_data=mysqli_query($conn,"SELECT*FROM ".$tablename);
-			if(!$table_data){
-				echo 'Could not run query: '. mysqli_error($conn);
-			}
+        /*-------------------------------------*/
+        //------Inserting Data SQL Start-------//
+        /*-------------------------------------*/
+        $all_table_data = array();
+        foreach ($this->tables($conn) as $key => $table) {
+            $show_field = $this->view_fields($conn, $table);
+            $solid_field_name = implode(", ", $show_field);
+            $create_field_sql = "INSERT INTO `$table` ( " . $solid_field_name . ") VALUES \n";
 
-			if(mysqli_num_rows($table_data)>0){
+            //Start checking data available
+            mysqli_query($conn, "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
+            $table_data = mysqli_query($conn, "SELECT*FROM " . $table);
+            if (!$table_data) {
+                echo 'Could not run query: ' . mysqli_error($conn);
+            }
 
-				
-				while ($t_data=mysqli_fetch_row($table_data)) {
+            if (mysqli_num_rows($table_data) > 0) {
+                $data_viewig = $this->view_data($conn, $table);
+                $splice_data = array_chunk($data_viewig, 50);
+                foreach ($splice_data as $each_datas) {
+                    $solid_data_viewig = implode(", \n", $each_datas) . "; ";
+                    $all_table_data[] = $create_field_sql . $solid_data_viewig;
+                }
 
-					$per_data=array();
-					foreach ($t_data as $key => $tb_data) {
-						$per_data[]= "'".str_replace("'","\'",$tb_data)."'";
-					}
-					$solid_data= "(".implode(", ",$per_data).")";
-					$all_data[]=$solid_data;
-				}
-			}
-				return $all_data;
-		}
-	
+            } else {
+                $all_table_data[] = null;
+            }
+            //End checking data available
 
-		/*-------------------------------------*/
-		//---------Functions End here----------//
-		/*-------------------------------------*/
 
-		//Export End here==================================================================
-		//Import Start here==================================================================
-		function db_import($conn,$file_path){
-			$tbl_query=null;
-			foreach ($this->tables($conn) as $key => $table) {
-				$tbl_query=mysqli_query($conn,"DROP TABLE IF EXISTS ".$table);
-			}
-		 
-			//---------------------------------------------------------------------------
-			//Forign code Start here
-			//---------------------------------------------------------------------------
-			$templine = '';
-			// Read in entire file
-			$lines = file($file_path);
-			// Loop through each line
-			foreach ($lines as $line)
-			{
-			// Skip it if it's a comment
-				if (substr($line, 0, 2) == '--' || $line == '')
-					continue;
+        }
+        $entiar_table_data = implode(" \n\n\n", $all_table_data);
+        /*-------------------------------------*/
+        //-------Inserting Data SQL End--------//
+        /*-------------------------------------*/
+        $this->exported_database = $solid_tablecreate_sql . "; \n \n" . $entiar_table_data;
+        return $this;
+    }
 
-				// Add this line to the current segment
-				$templine .= $line;
-				// If it has a semicolon at the end, it's the end of the query
-				if (substr(trim($line), -1, 1) == ';')
-				{
-					// Perform the query
-					mysqli_query($conn,$templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysqli_error($conn) . '<br /><br />');
-					// Reset temp variable to empty
-					$templine = '';
-				}
-			}
+    /**
+     * @param $conn The link of connection
+     * @param $fileName the name of csv
+     * @param the name of table is imported
+     * @return int return number lines are imported into tables;
+     */
+    public function importFromCsv($conn, $fileName, $database_table)
+    {
+        $values_array = array();
+        $input = fopen($fileName, 'a+');
+        $input2 = fopen($fileName, 'a+');
+        $first_row = fgetcsv($input, 1024, ';');
+        foreach ($first_row as $name) {
+            $values_array[] = ':' . trim($name);
+        }
+        $columns = implode(', ', fgetcsv($input2, 1024, ';'));
+        $values = implode(', ', $values_array);
 
-			 //echo "Database imported successfully <br/>";
-			return true;
-		}
-	}
+        $count = 0;
+        while ($row = fgetcsv($input, 1024, ';')) {
+            $sql = "INSERT INTO $database_table($columns) VALUES($values)";
+            $query = $conn->prepare($sql);
+            for ($i = 0; $i < count($row); $i++) {
+
+                $query->bindParam($values_array[$i], $row[$i]);
+            }
+            $query->execute();
+            $count++;
+        }
+        return $count;
+    }
+    //Additional Methods
+    /*-------------------------------------*/
+    //--------Functions Start here---------//
+    /*-------------------------------------*/
+
+    public function download($name = 'backup')
+    {
+        /*//Download
+        $file_name="Tmpdata.sql";
+        $file=fopen($file_name,"w+");
+        fwrite($file, $this->exported_database);*/
+
+        header('Content-Type: application/sql');
+        header('Content-Disposition: attachment; filename=' . $name . '.sql');
+        echo $this->exported_database;
+        /*readfile($file_name);
+        fclose($file);
+        unlink($file_name);*/
+    }
+
+    public function save($path, $name = "")
+    {
+        $name = ($name != "")? $name : 'backup_' . date('Y-m-d');
+
+        //Save file
+        $file = fopen($path . $name . ".sql", "w+");
+        $fw = fwrite($file, $this->exported_database);
+        if (!$fw) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function connect($server, $user, $pass, $db)
+    {
+        $conn = mysqli_connect($server, $user, $pass, $db);
+
+        if (!$conn) {
+            echo mysqli_error($conn);
+        }
+        return $conn;
+
+    }
+
+    public function tables($conn)
+    {
+        /*-------------------------------------*/
+        //------Creating Table List start------//
+        /*-------------------------------------*/
+        $tb_name = mysqli_query($conn, "SHOW TABLES");
+        $tables = array();
+        while ($tb = mysqli_fetch_row($tb_name)) {
+            $tables[] = $tb[0];
+        }
+        /*-------------------------------------*/
+        //-------Creating Table List end-------//
+        /*-------------------------------------*/
+        return $tables;
+    }
+
+    public function view_fields($conn, $tablename)
+    {
+        $all_fields = array();
+        $fields = mysqli_query($conn, "SHOW COLUMNS FROM " . $tablename);
+        if (!$fields) {
+            echo 'Could not run query: ' . mysqli_error($conn);
+        }
+
+        if (mysqli_num_rows($fields) > 0) {
+            while ($field = mysqli_fetch_assoc($fields)) {
+                $all_fields[] = "`" . $field["Field"] . "`";
+            }
+        }
+        return $all_fields;
+    }
+
+
+    public function view_data($conn, $tablename)
+    {
+        $all_data = array();
+        $table_data = mysqli_query($conn, "SELECT*FROM " . $tablename);
+        if (!$table_data) {
+            echo 'Could not run query: ' . mysqli_error($conn);
+        }
+
+        if (mysqli_num_rows($table_data) > 0) {
+
+
+            while ($t_data = mysqli_fetch_row($table_data)) {
+
+                $per_data = array();
+                foreach ($t_data as $key => $tb_data) {
+                    $per_data[] = "'" . str_replace("'", "\'", $tb_data) . "'";
+                }
+                $solid_data = "(" . implode(", ", $per_data) . ")";
+                $all_data[] = $solid_data;
+            }
+        }
+        return $all_data;
+    }
+
+
+    /*-------------------------------------*/
+    //---------Functions End here----------//
+    /*-------------------------------------*/
+
+    //Export End here==================================================================
+    //Import Start here==================================================================
+    function db_import($conn, $file_path)
+    {
+        $tbl_query = null;
+        foreach ($this->tables($conn) as $key => $table) {
+            $tbl_query = mysqli_query($conn, "DROP TABLE IF EXISTS " . $table);
+        }
+
+        //---------------------------------------------------------------------------
+        //Forign code Start here
+        //---------------------------------------------------------------------------
+        $templine = '';
+        // Read in entire file
+        $lines = file($file_path);
+        // Loop through each line
+        foreach ($lines as $line) {
+            // Skip it if it's a comment
+            if (substr($line, 0, 2) == '--' || $line == '')
+                continue;
+
+            // Add this line to the current segment
+            $templine .= $line;
+            // If it has a semicolon at the end, it's the end of the query
+            if (substr(trim($line), -1, 1) == ';') {
+                // Perform the query
+                mysqli_query($conn, $templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysqli_error($conn) . '<br /><br />');
+                // Reset temp variable to empty
+                $templine = '';
+            }
+        }
+
+        //echo "Database imported successfully <br/>";
+        return true;
+    }
+}
